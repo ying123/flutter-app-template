a1c1d0c744e9181b21b469be83e9c81de77bf905
canknow <3230525823@qq.com>
2020-03-01 12:04:04 +0800

    optimize: optimize update style

--- lib/views/components/UpdateDialog.dart
+++ lib/views/components/UpdateDialog.dart
@@ -6,7 +6,6 @@ import 'package:canknow_flutter_ui/utils/FileUtil.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/apis/commonFetch.dart';
-import 'package:canknow_flutter_ui/components/ApplicationDialogHeader.dart';
 import 'package:canknow_flutter_ui/components/TextButton.dart';
 import 'package:canknow_flutter_ui/components/toast/Toast.dart';
 import 'package:canknow_flutter_ui/styles/variables.dart';
@@ -35,6 +34,67 @@ class UpdateDialogState extends State<UpdateDialog> {
   CancelToken token;
   UpdateStatus updateStatus = UpdateStatus.idle;
 
+  buildHeader() {
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            child: Image.asset('assets/images/update-header-background.png'),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: Variables.contentPaddingLargest,
+            child: Center(
+              child: Text(widget.version, style: TextStyle(color: Colors.white),),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  buildFooter() {
+    return Container(
+      padding: EdgeInsets.all(Variables.contentPadding),
+      child: Row(
+        children: <Widget>[
+          if(!widget.isForce) Expanded(
+            flex: 1,
+            child: TextButton(text: ApplicationLocalizations.of(context).text('common.dialog.cancel'),
+                circle: true,
+                border: true,
+                onTap: () {
+                  Navigator.of(context).pop();
+                }),
+          ),
+          if(!widget.isForce) Spaces.hComponentSpan,
+          Expanded(
+            flex: 1,
+            child: TextButton(
+              disabled: updateStatus == UpdateStatus.updating,
+              text: ApplicationLocalizations.of(context).text('update.updateRightNow'),
+              border: true,
+              circle: true,
+              scene: Scene.error,
+              onTap: () async {
+                updateStatus = UpdateStatus.updating;
+                if (mounted)
+                  setState(() {
+                  });
+                if (Platform.isAndroid) {
+                  _androidUpdate();
+                }
+                else if (Platform.isIOS) {
+                  _iosUpdate();
+                }
+              },),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -45,57 +105,32 @@ class UpdateDialogState extends State<UpdateDialog> {
           child: IntrinsicHeight(
             child: Container(
               margin: EdgeInsets.all(Variables.componentSpan),
-              decoration: ShapeDecoration(
-                color: Colors.white,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(Variables.blockBorderRadius),)),
-              ),
               child: Column(
                 children: <Widget>[
+                  buildHeader(),
                   Container(
-                    padding: EdgeInsets.all(Variables.contentPadding),
-                    alignment: Alignment.topLeft,
-                    child: Material(
-                      color: Colors.transparent,
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.vertical,
-                        child: Text(widget.updateInfo ?? "", style: TextStyle(color: Colors.black, fontSize: Variables.fontSizeSmall),),
-                      ),
+                    decoration: ShapeDecoration(
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.vertical(bottom: Radius.circular(Variables.borderRadiusLarger),)),
                     ),
-                  ),
-                  _buildLoading(),
-                  Container(
-                    padding: EdgeInsets.all(Variables.contentPadding),
-                    child: Row(
+                    child: Column(
                       children: <Widget>[
-                        if(!widget.isForce) Expanded(
-                          flex: 1,
-                          child: TextButton(text: ApplicationLocalizations.of(context).text('common.dialog.cancel'),
-                              ghost: true,
-                              onTap: () {
-                                Navigator.of(context).pop();
-                              }),
+                        // 更新信息
+                        Container(
+                          padding: EdgeInsets.all(Variables.contentPadding),
+                          alignment: Alignment.topLeft,
+                          child: Material(
+                            color: Colors.transparent,
+                            child: SingleChildScrollView(
+                              scrollDirection: Axis.vertical,
+                              child: Text(widget.updateInfo ?? "", style: TextStyle(color: Colors.black, fontSize: Variables.fontSizeSmall),),
+                            ),
+                          ),
                         ),
-                        if(!widget.isForce) Spaces.hComponentSpan,
-                        Expanded(
-                          flex: 1,
-                          child: TextButton(
-                            disabled: updateStatus == UpdateStatus.updating,
-                            text: ApplicationLocalizations.of(context).text('update.update'),
-                            border: true,
-                            scene: Scene.error,
-                            onTap: () async {
-                              updateStatus = UpdateStatus.updating;
-                              if (mounted)
-                                setState(() {
-                                });
-                              if (Platform.isAndroid) {
-                                _androidUpdate();
-                              }
-                              else if (Platform.isIOS) {
-                                _iosUpdate();
-                              }
-                            },),
-                        ),
+                        // 进度条
+                        if(updateStatus == UpdateStatus.updating) _buildProgress(),
+                        // 底部按钮
+                        buildFooter()
                       ],
                     ),
                   )
@@ -148,22 +183,16 @@ class UpdateDialogState extends State<UpdateDialog> {
     }
   }
 
-  Widget _buildLoading() {
-    if (updateStatus == UpdateStatus.updating) {
-      return Expanded(
-        child: Container(
-          padding: EdgeInsets.all(Variables.contentPadding),
-          child: LinearProgressIndicator(
-            valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor),
-            backgroundColor: Colors.grey[300],
-            semanticsLabel: _downloadProgress.toString(),
-            value: _downloadProgress / 100,
-          ),
-        ),
-        flex: 1,
-      );
-    }
-    return Container();
+  Widget _buildProgress() {
+    return Container(
+      padding: EdgeInsets.all(Variables.contentPadding),
+      child: LinearProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor),
+        backgroundColor: Colors.grey[300],
+        semanticsLabel: _downloadProgress.toString(),
+        value: _downloadProgress / 100,
+      ),
+    );
   }
 
   void _iosUpdate() {
